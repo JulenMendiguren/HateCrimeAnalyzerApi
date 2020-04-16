8df0ce4b233c5f93a92f60b4bc346e01bc4affe7
JulenMendiguren <mendihyk@hotmail.com>
2020-04-16 19:49:13 +0200

    Cambios para hacer las preguntas multiidoma y con tags

--- models/question.js
+++ models/question.js
@@ -1,14 +1,31 @@
 const mongoose = require('mongoose');
 
 const questionSchema = new mongoose.Schema({
-    text: {
+    text_eu: {
         type: String,
-        required: true
+        required: true,
+    },
+    text_es: {
+        type: String,
+        required: true,
+    },
+    text_en: {
+        type: String,
+        required: true,
+    },
+    text_fr: {
+        type: String,
+        required: true,
     },
     category: {
         type: String,
         enum: ['user', 'report'],
-        required: true
+        required: true,
+    },
+    tag: {
+        type: String,
+        enum: ['all', '01', '02', '03', '04', '05', '06', '07', '08'],
+        required: true,
     },
     type: {
         type: String,
@@ -20,35 +37,44 @@ const questionSchema = new mongoose.Schema({
             'yesno',
             'radio',
             'multiselect',
-            'geolocation'
+            'geolocation',
         ],
-        required: true
+        required: true,
     },
-    possibleAnswers: {
-        type: [String]
+    possibleAnswers_eu: {
+        type: [String],
+    },
+    possibleAnswers_es: {
+        type: [String],
+    },
+    possibleAnswers_en: {
+        type: [String],
+    },
+    possibleAnswers_fr: {
+        type: [String],
     },
     options: {
         required: {
             type: Boolean,
-            required: true
+            required: true,
         },
         subquestionOf: mongoose.Schema.Types.ObjectId,
-        requiredAnswer: String,
+        requiredAnswerIndex: Number, // Índice de la respuesta requerida en possibleAnswers_xx
         size: {
             type: String, // Sólo textbox
-            enum: ['big', 'small']
+            enum: ['big', 'small'],
         },
         minLength: Number, // Sólo textbox, número de caracteres
         maxLength: Number, // Sólo textbox, número de caracteres
         minValue: Number, // Sólo number, valor mínimo
         maxValue: Number, // Sólo number, valor máximo
-        slider: Boolean, // Sólo number, true or false (absencia == false), necesita maxValue y minValue
+        slider: Boolean, // Sólo number, true or false (ausencia == false), necesita maxValue y minValue
         datetimeFormat: {
             type: String, // Sólo textbox
-            enum: ['datetime', 'date', 'time']
-        } // Sólo datetime, muestra fecha y hora, sólo fecha, o sólo hora.
+            enum: ['datetime', 'date', 'time'],
+        }, // Sólo datetime, muestra fecha y hora, sólo fecha, o sólo hora.
     },
-    createdAt: { type: Date, default: Date.now } // Fecha de creación
+    createdAt: { type: Date, default: Date.now }, // Fecha de creación
 });
 
 const Question = mongoose.model('questions', questionSchema);
